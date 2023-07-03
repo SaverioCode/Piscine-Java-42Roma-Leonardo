7b118163796040ea1e64992cc19aadd9d5a8f63c
Francesco Saverio Garzi Cosentino <fgarzi-c@e4r11p1.42roma.it>
2023-07-03 13:56:04 +0200

    update

---
+++ module_00/ex01/Program.java
@@ -0,0 +1,28 @@
+
+import  java.util.Scanner;
+
+class Program {
+    public static void main(String[] args) {
+        int num;
+        int i;
+
+        System.out.print("Insert a number: ");
+        Scanner getNum = new Scanner(System.in);
+        num = getNum.nextInt();
+        if (num < 2) {
+            System.err.println("Illegal argument");
+            System.exit(-1);
+        }
+        i = 2;
+        while ((i * i) <= num) {
+            if (num % i == 0) {
+                System.out.print("false ");
+                System.out.println(i - 1);
+                System.exit(0);
+            }
+            i++;
+        }
+        System.out.print("true ");
+        System.out.println(i - 2);
+    }    
+}
