b999378d81e7650130d46417da078957d81587c1
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r3p7.42roma.it>
2023-07-05 20:56:03 +0200

    update

---
+++ module_00/ex04/Program.java
@@ -0,0 +1,99 @@
+
+import java.util.Scanner;
+
+class Program {
+	static int[][] insertArray(int[][] top10, int index, int[] arr) {
+		int		i;
+		int[]	tmp1;
+		int[]	tmp2;
+
+		tmp1 = top10[index];
+		top10[index] = arr;
+		i = index + 1;
+		while (i < 9) {
+			tmp2 = top10[i];
+			top10[i] = tmp1;
+			i++;
+		}
+		return (old);
+	}
+
+	static int[][] updateTop10(int[][] top10, int[] arr) {
+		byte	i;
+
+		i = 9;
+		while (i >= 0) {
+			if (top10[i][1] <= arr[1]) {
+				top10 = insertArray(top10, i, arr);
+			}
+			i--;
+		}
+		return (top10);
+	}
+
+	static int[][] initTop10(int[][] top10) {
+		byte	i;
+
+		i = 0;
+		while (i < 10) {
+			top10[i][0] = 0;
+			top10[i][1] = 0;
+			i++;
+		}
+		return (top10);
+	}
+
+	static int[][] getTop10(int[] unicode) {
+		int		len;
+		int		lowest;
+		int		i;
+		int[][]	top10 = new int[10][2];
+		
+		lowest = 1;
+		len = 0;
+		i = 0;
+		top10 = initTop10(top10);
+		while (i < 65536) {
+			if (unicode[i] >= lowest) {
+				top10 = updateTop10(top10, unicode[i]);
+				lowest = updateLowest();
+			}
+			i++;
+		}
+	}
+
+	static int[] updateUnicode(char[] arrOfChar, int len) {
+		int[]	unicode = new int[65536];
+		int		i;
+		int		index;
+
+		i = 0;
+		while (i < len) {
+			index = (int) arrOfChar[i];
+			unicode[index] += 1;
+			if (unicode[index] > 999) {
+				System.err.println("Maximum character of the same type is 999.");
+				System.exit(-1);
+			}
+			i++;
+		}
+		return (unicode);
+	}
+
+	public static void main(String[] args) {
+		String	input;
+		char[]	arrOfChar;
+		int		len;
+		int[]	unicode;
+		int[][]	top10;
+	
+		Scanner getInput = new Scanner(System.in);
+		input = getInput.nextLine();
+		getInput.close();
+		len = input.length();
+		arrOfChar = input.toCharArray();
+		unicode = updateUnicode(arrOfChar, len);
+		top10 = getTop10(unicode);
+		/// print chart ///
+	}
+}
