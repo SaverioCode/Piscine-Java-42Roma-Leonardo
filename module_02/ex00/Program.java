b010de6acd42498964b61adc035305d3fbe16f0b
sav <sav@debian-BULLSEYE-live-builder-AMD64>
2023-08-18 11:48:53 +0200

    update

--- module_02/ex00/Program.java
+++ module_02/ex00/Program.java
@@ -1,37 +1,96 @@
 
 import java.io.*;
-import java.util.Scanner;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Program {
 
-	private static void readSignature(List<String> fileList, List<int[]> signature) {
-		InputStream	file;
-		int	data[] = new int[4];
+	private static void	printResults(List<String> results) {
+		for (String str: results) {
+			System.out.println(str);
+		}
+	}
 
-		for (String fileName : fileList) {
+	private static String[] readSignature(String fileName) {
+		InputStream	file;
+		String	data[] = new String[4];
+
+		try {
+			file = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+			return (null);
+		}
+		for (int i = 0; i < 4; i++) {
 			try {
-				file = new FileInputStream(fileName);
-			} catch (FileNotFoundException e) {
-				System.err.println(e);
-				continue ;
-			}
-			for (int i = 0; i < 4; i++) {
-				try {
-					data[i] = file.read();
-				} catch (IOException e) {
-					System.err.println(e);
-					break ;
-				}
-			}
-			try {
-				file.close();
+				data[i] = Integer.toHexString(file.read());
 			} catch (IOException e) {
 				System.err.println(e);
+				break ;
 			}
 		}
+		try {
+			file.close();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		return (data);
+	}
 
-		//Integer.toHexString(data));
+	/// TESTING ///
+	private static void	printData(String[] data) {
+		System.out.println("PRINT DATA 1");
+		for (String str: data) {
+			System.out.println(str);
+		}
+		System.out.println("END PRINT DATA\n");
+	}
+
+	private static byte	check(List<String> results, String[] values, String[] data) {
+		System.out.println("\nCHECK");
+		for (int i = 0; i < 4; i++) {
+			System.out.printf("value: |%s|, data: |%s|\n", values[i], data[i] );////////////////
+			if (values[i].equals(data[i]) == false) {
+				System.out.println("FAILED\n");
+				// System.out.println(fileName + "FAILED");/////////////
+				return (1);
+			}
+		}
+		System.out.println("SUCCESS\n");
+		return (0);
+	}
+
+	private static List<String> matchSignature(Map<String, String[]> database, List<String> fileList) {
+		List<String>	results;
+		String[]		data;
+
+		results = new ArrayList<String>();
+		for (String fileName: fileList) {
+			data = readSignature(fileName);
+			if (data == null) {
+				System.out.println("DATA IS NULL");///////////
+				continue ;
+			}
+			printData(data);
+			// if (database.containsValue(data) == true) {
+				System.out.println("FUCKING HERE");///////////////
+				for (Entry<String, String[]> entry: database.entrySet()) {
+					String[] values = entry.getValue();
+					// String[] values = entry.getValue();
+					// for (int i = 0; i < 4; i++) {
+					// 	System.out.println(values[i]);////////////////
+					// 	if (values[i].equals(data[i]) == false) {
+					// 		System.out.println(fileName + "FAILED");/////////////
+					// 		continue;
+					// 	}
+					// }
+					if (check(results, values, data) == 0) {
+						results.add(fileName + "\n" + entry);
+					}
+				}
+			// }
+		}
+		return (results);
 	}
 
 	private static void	getFilesName(List<String> fileName) {
@@ -45,14 +104,23 @@ public class Program {
 				return ;
 			}
 			fileName.add(input);
+			System.out.println("PROCESSED");
 		}
+	}
+
+	/// TESTING ///
+	private static void	printFileNames(List<String> fileList) {
+		System.out.println("\nPRINT FILE NAMES");
+		for (String fileName: fileList) {
+			System.out.println(fileName);
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
 		Map<String, String[]>	database;
-		List<int[]> 			signature;
 		List<String>			fileName;
-		/// create list with results ///
+		List<String>			results;
 
 		database = new HashMap<String, String[]>();
 		try {
@@ -63,11 +131,8 @@ public class Program {
 		}
 		fileName = new ArrayList<String>();
 		getFilesName(fileName);
-		signature = new ArrayList<int[]>();
-		readSignature(fileName, signature);
-		// match signatures and store result
-		// print results
-
-		// end program
+		printFileNames(fileName);//////TESTING///////
+		results = matchSignature(database, fileName);
+		printResults(results);
 	}
 }
