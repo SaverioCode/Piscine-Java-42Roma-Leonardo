5dffde579a6fb5d150e1e7ff110a81f4dd22906b
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r11p3.42roma.it>
2023-08-19 22:11:25 +0200

    update

--- module_02/ex01/Program.java
+++ module_02/ex01/Program.java
@@ -1,6 +1,27 @@
 
+import	java.io.Reader;
+import	java.io.FileReader;
+import	java.io.IOException;
+import	java.io.FileNotFoundException;
+import	java.util.TreeSet;
+
 public class	Program {
+
 	public static void	main(String[] args) {
+		TreeSet<String>	dictionary;
+
+		if (args.length != 2) {
+			System.err.println("Error: number of arguments has to be 2.");
+			System.exit(1);
+		}
+		/// check size of the files
+
+		// dictionary = createDictionary(args);
+		dictionary = Dictionary.createDictionary(args);
+		/// print dictionary in dictionary.txt
+		
+		/// match worlds
+		/// end program
 
 	}
 }
