bf2feeca5f206fe2030c49a8437b5c87421cd0e4
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r11p3.42roma.it>
2023-08-20 03:47:15 +0200

    udpate

---
+++ module_02/ex01/CompareFiles.java
@@ -0,0 +1,145 @@
+
+
+import	java.lang.Math;
+import	java.io.FileReader;
+import	java.io.FileWriter;
+import	java.io.IOException;
+import	java.io.FileNotFoundException;
+import	java.util.List;
+import	java.util.ArrayList;
+import	java.util.Iterator;
+import	java.util.TreeSet;
+
+public class	CompareFiles {
+	private static CompareFiles		instance;
+	public static TreeSet<String>	dictionary;
+	private static List<String>		wordsList1;
+	private static List<String>		wordsList2;
+	private static int[]			vector1;
+	private static int[]			vector2;
+	public static double			similarity;
+
+	private CompareFiles(String[] files) {
+		this.wordsList1 = new ArrayList<String>();
+		this.wordsList2 = new ArrayList<String>();
+		this.dictionary = createDictionary(files);
+		this.vector1 = createVector(this.wordsList1);
+		this.vector2 = createVector(this.wordsList2);
+		this.similarity = compareVectors();
+	}
+
+	private double	compareVectors() {
+		double	numerator, denominator;
+		double	a, b, sommatoryA, sommatoryB;
+
+		numerator = denominator = sommatoryA = sommatoryB= 0;
+		for (int i = 0; i < dictionary.size(); i++) {
+			a = this.vector1[i];
+			b = this.vector2[i];
+			numerator += a * b;
+			sommatoryA += Math.pow(a, 2);
+			sommatoryB += Math.pow(b, 2);
+		}
+		denominator = Math.sqrt(sommatoryA) * Math.sqrt(sommatoryB);
+		return (numerator / denominator);
+	}
+
+	public static CompareFiles	getInstance(String[] files) {
+		if (instance == null) {
+			instance = new CompareFiles(files);
+		}
+		return (instance);
+	}
+
+	public void		printDictionary() throws IOException{
+		FileWriter			file = null;
+		String				word;
+		Iterator<String>	list;
+
+		file = new FileWriter("dictionary.txt");
+		list = this.dictionary.iterator();
+		while (list.hasNext()) {
+			word = list.next();
+			file.write(word + " ");
+		}
+		file.close();
+	}
+
+	private int[]	createVector(List<String> wordList) {
+		Iterator<String>	list;
+		String				word;
+		int[]				vector;
+		int					i;
+
+		vector = new int[this.dictionary.size()];
+		list = this.dictionary.iterator();
+		i = 0;
+		while (list.hasNext()) {
+			word = list.next();
+			while (wordList.indexOf(word) != -1) {
+					wordList.remove(word);
+					vector[i] += 1;
+			}
+			i++;
+		}
+		wordList = null;
+		return (vector);
+	}
+
+	private	static String	getWord(FileReader file) {
+		String	word = "";
+		int		character;
+
+		while (true) {
+			try {
+				character = file.read();
+			} catch (IOException e) {
+				System.err.println(e);
+				break ;
+			}
+			if (character == -1) {
+				break ;
+			}
+			if (character < 33 || character == 127) {
+				if (word.length() == 0) {
+					continue ;
+				}
+				break ;
+			}
+			word += (char)character;
+		}
+		if (word.length() == 0) {
+			return (null);
+		}
+		return (word);
+	}
+
+	private static void	addWordList(String fileName, List<String> wordList) {
+		FileReader		file = null;
+
+		try {
+			file = new FileReader(fileName);
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+		for (String word = getWord(file); word != null; word = getWord(file)) {
+			wordList.add(word);
+			dictionary.add(word);
+		}
+		try {
+			file.close();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	private TreeSet<String> createDictionary(String[] files) {
+		FileReader		file = null;
+
+		this.dictionary = new TreeSet<String>();
+		addWordList(files[0], this.wordsList1);
+		addWordList(files[1], this.wordsList2);
+		return (dictionary);
+	}
+}
--- module_02/ex01/Program.java
+++ module_02/ex01/Program.java
@@ -1,27 +1,51 @@
 
-import	java.io.Reader;
-import	java.io.FileReader;
 import	java.io.IOException;
-import	java.io.FileNotFoundException;
+import	java.lang.Math;
+import	java.math.BigDecimal;
+import	java.nio.file.Files;
+import	java.nio.file.Path;
+import	java.nio.file.Paths;
 import	java.util.TreeSet;
 
 public class	Program {
 
-	public static void	main(String[] args) {
-		TreeSet<String>	dictionary;
+	private static void	checkInput(String[] files) {
+		Path	path;
+		long	fileSize = 0;
 
-		if (args.length != 2) {
+		if (files.length != 2) {
 			System.err.println("Error: number of arguments has to be 2.");
 			System.exit(1);
 		}
-		/// check size of the files
+		for (String file: files) {
+			path = Paths.get(file);
+			try {
+				fileSize = Files.size(path) / Math.round(Math.pow(10, 6));
+			}	catch (IOException e) {
+				System.err.println(e);
+				System.exit(1);
+			}
+			if (fileSize > 10) {
+				System.err.println("Error: file is bigger then 10Mb.");
+				System.exit(1);
+			}
+		}
+	}
 
-		// dictionary = createDictionary(args);
-		dictionary = Dictionary.createDictionary(args);
-		/// print dictionary in dictionary.txt
-		
-		/// match worlds
-		/// end program
-
+	public static void	main(String[] args) {
+		CompareFiles	instance;
+		BigDecimal		similarity;
+	
+		checkInput(args);
+		instance = CompareFiles.getInstance(args);
+		try {
+			instance.printDictionary();
+		} catch (IOException e) {
+			System.err.println(e);
+			System.exit(1);
+		}
+		similarity = new BigDecimal((float)instance.similarity);
+		similarity = similarity.setScale(2, similarity.ROUND_DOWN);
+		System.out.println("Similarity = " + similarity);
 	}
 }
