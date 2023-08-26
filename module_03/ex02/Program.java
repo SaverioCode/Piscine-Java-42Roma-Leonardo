043e91530fcffd853d6d1efc4b1054c90b7549bd
Francesco Saverio Garzi Cosentino <fgarzi-c@e3r10p1.42roma.it>
2023-08-26 02:17:15 +0200

    update

---
+++ module_03/ex02/Program.java
@@ -0,0 +1,37 @@
+
+public class	Program {
+	private	static int	arraySize;
+	private static int	threadsCount;
+
+	private static void	exitError(String str) {
+		System.err.println(str);
+		System.exit(1);
+	}
+
+	private static void	checkInput(String[] args) {
+		String[]	input;
+
+		if (args.length != 2) {
+			exitError("Error: the program input \"--arraySize=<INTEGER> --threadsCount=<INTEGER>\".");
+		}
+		input = args[0].split("=", 2);
+		if (input.length != 2 || input[0].equals("--arraySize") == false) {
+			exitError("Error: input has to be \"--arraySize=<INTEGER>\".");
+		}
+		arraySize = Integer.parseInt(input[1]);
+		input = args[1].split("=", 2);
+		if (input.length != 2 || input[0].equals("--threadsCount") == false) {
+			exitError("Error: input has to be \"--threadsCount=<INTEGER>\".");
+		}
+		threadsCount = Integer.parseInt(input[1]);
+	}
+
+	public static void	main(String[] args) {
+		
+		try {
+			checkInput(args);
+		} catch (NumberFormatException e) {
+			exitError(e);
+		}
+	}
+}
